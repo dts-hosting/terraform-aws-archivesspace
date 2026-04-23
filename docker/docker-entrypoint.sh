da8d4af3dda21848a65b245170bbf6c796bdff1e
Blake Carver <btcarver@lisnews.com>
2026-04-23 11:30:50 -0400

    fixes missing fail to redirect on pui

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env sh
 set -eu
 
-envsubst '${API_IPS_ALLOWED} ${API_PREFIX} ${OAI_PREFIX} ${PUBLIC_NAME} ${PUBLIC_PREFIX} ${PUI_IPS_ALLOWED} ${REAL_IP_CIDR} ${STAFF_NAME} ${STAFF_PREFIX} ${SUI_IPS_ALLOWED} ${UPSTREAM_HOST}' \
-  < /etc/nginx/conf.d/$PROXY_TYPE-domain.conf.template > /etc/nginx/conf.d/default.conf
+if [ "$PUBLIC_PREFIX" != "/" ]; then
+  REDIRECT_BLOCK="location = ${PUBLIC_PREFIX%/} { return 301 \$scheme://\$host$PUBLIC_PREFIX\$is_args\$args; }"
+else
+  REDIRECT_BLOCK=""
+fi
 
+export REDIRECT_BLOCK
+
+envsubst '${API_IPS_ALLOWED} ${API_PREFIX} ${OAI_PREFIX} ${PUBLIC_NAME} ${PUBLIC_PREFIX} ${PUI_IPS_ALLOWED} ${REAL_IP_CIDR} ${STAFF_NAME} ${STAFF_PREFIX} ${SUI_IPS_ALLOWED} ${UPSTREAM_HOST} ${REDIRECT_BLOCK}' \
+  < /etc/nginx/conf.d/$PROXY_TYPE-domain.conf.template > /etc/nginx/conf.d/default.conf
 exec "$@"
