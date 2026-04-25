df403a82d373688f4dc5f801b8d72a997615f9ad
Blake Carver <btcarver@lisnews.com>
2026-04-25 11:51:21 -0400

    add root level redirect to prefix

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,12 +3,15 @@ set -eu
 
 if [ "$PUBLIC_PREFIX" != "/" ]; then
   REDIRECT_BLOCK="location = ${PUBLIC_PREFIX%/} { return 301 \$scheme://\$host$PUBLIC_PREFIX\$is_args\$args; }"
+  ROOT_REDIRECT_BLOCK="location = / { return 301 \$scheme://\$host$PUBLIC_PREFIX; }"
 else
   REDIRECT_BLOCK=""
+  ROOT_REDIRECT_BLOCK=""
 fi
 
 export REDIRECT_BLOCK
+export ROOT_REDIRECT_BLOCK
 
-envsubst '${API_IPS_ALLOWED} ${API_PREFIX} ${OAI_PREFIX} ${PUBLIC_NAME} ${PUBLIC_PREFIX} ${PUI_IPS_ALLOWED} ${REAL_IP_CIDR} ${STAFF_NAME} ${STAFF_PREFIX} ${SUI_IPS_ALLOWED} ${UPSTREAM_HOST} ${REDIRECT_BLOCK}' \
+envsubst '${API_IPS_ALLOWED} ${API_PREFIX} ${OAI_PREFIX} ${PUBLIC_NAME} ${PUBLIC_PREFIX} ${PUI_IPS_ALLOWED} ${REAL_IP_CIDR} ${STAFF_NAME} ${STAFF_PREFIX} ${SUI_IPS_ALLOWED} ${UPSTREAM_HOST} ${REDIRECT_BLOCK} ${ROOT_REDIRECT_BLOCK}' \
   < /etc/nginx/conf.d/$PROXY_TYPE-domain.conf.template > /etc/nginx/conf.d/default.conf
 exec "$@"
